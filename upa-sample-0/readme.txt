a7066982d966dd977463eff9603fb591b9af2f9d
Pablo Grisafi <pablogrisafi1975@gmail.com>
2015-04-12 07:17:21 -0300

    First commit

---
+++ upa-sample-0/readme.txt
@@ -0,0 +1,5 @@
+mvn eclipse:eclipse -Dwtpversion=2.0 
+
+http://localhost:8080/upa-sample-0/hello
+
+http://localhost:8080/upa-sample-0/index.jsp
---
+++ upa-sample-0/src/main/java/org/upasample0/HelloServlet.java
@@ -0,0 +1,20 @@
+package org.upasample0;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(value = "/hello", name = "helloServlet")
+public class HelloServlet extends GenericServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		res.getWriter().println("Hello world!! servlet");
+	}
+}
